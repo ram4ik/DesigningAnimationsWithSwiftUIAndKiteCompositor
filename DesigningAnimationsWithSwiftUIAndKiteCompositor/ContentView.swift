1216f89cde53eb013d86576be99f021e3feaf63a
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-21 20:01:45 +0300

    init

--- DesigningAnimationsWithSwiftUIAndKiteCompositor/ContentView.swift
+++ DesigningAnimationsWithSwiftUIAndKiteCompositor/ContentView.swift
@@ -9,8 +9,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
     var body: some View {
-        Text("Hello World")
+        Image(systemName: "heart.fill")
+            .foregroundColor(.red)
+            .imageScale(.large)
+            .scaleEffect(show ? 4 : 1)
+            .animation(
+                .interpolatingSpring(
+                    mass: 0.7,
+                    stiffness: 200,
+                    damping: 10,
+                    initialVelocity: 4
+                ))
+            .onTapGesture {
+
+                self.show.toggle()
+        }
     }
 }
 
